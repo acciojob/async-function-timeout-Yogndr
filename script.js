94046c1dcc9ada4c419d9ea2e35a1f31a54f682d
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-06-01 12:23:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let text=document.querySelector("#text");
+let delay=document.querySelector("#delay");
+let button=document.querySelector("#button");
+let output=document.querySelector("#output");
+
+
+button.addEventListener("click",()=>{
+	async function sol(()=>{
+
+	   await setTimeout(()=>{
+		  output.textContent=text.value;
+	  },delay)
+	  
+  })
+})
+  
