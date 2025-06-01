36842c9c88a47118f429b072649818902ed333dc
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-06-01 12:56:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //your JS code here. If required.
-let text=document.querySelector("#text").value;
-let delay=parseInt(document.querySelector("#delay").value);
+
 let button=document.querySelector("#button");
 let output=document.querySelector("#output");
 
 
 button.addEventListener("click",async()=>{
-	
+
+	let text=document.querySelector("#text").value;
+let delay=parseInt(document.querySelector("#delay").value);
 	  output.textContent="";
 	await new Promise((resolve)=>{
 
